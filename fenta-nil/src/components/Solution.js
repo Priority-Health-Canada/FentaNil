a8e49d4d889ce34f5b7c2cfc3d0217d8ebab3c76
Aqeel Mozumder <aqeelmozumder@hotmail.com>
2024-03-19 09:40:20 -0700

    v3.0

--- fenta-nil/src/components/Solution.js
+++ fenta-nil/src/components/Solution.js
@@ -5,6 +5,7 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../assets/Solution.css'; // Make sure you import the Solution CSS for custom styling
+import '../assets/theme.css';
 
 function Solution() {
     useEffect(() => {
@@ -15,26 +16,40 @@ function Solution() {
     }, []);
 
     return (
-        <div className="container py-5">
+        <div className="container-fluid px-0 py-5">
             <div className="row justify-content-center mb-5" data-aos="fade-up">
                 <div className="col-12 text-center">
                     <h1 className="display-4">FENTA-NIL: Zeroing in on Toxic Drug Deaths</h1>
                 </div>
             </div>
-            <div className="row align-items-center mb-5">
-                <div className="col-lg-4" data-aos="fade-right">
-                    <p className="lead fs-3">A groundbreaking solution designed to empower individuals</p>
+            <div className="row align-items-center mb-1">
+                <div className="col-lg-5 offset-lg-1" data-aos="fade-right"> {/* Adjust the offset to match the gap */}
+                    <p className="lead fs-1 custom-padding-left">An Innovative fusion of Technology and Prowess in detecting Fentanyl</p>
                 </div>
-                <div className="col-lg-8 text-center" data-aos="fade-left">
+                <div className="col-lg-6 text-center" data-aos="fade-left">
                     <img src="/3Dprint1.png" alt="Fenta-Nil 3D Model 1" className="solution-image1" />
                 </div>
             </div>
-            <div className="row align-items-center">
-                <div className="col-lg-6 text-center" data-aos="fade-right">
+
+            <div className="new-section">
+              <div className="container-fluid px-0">
+                <div className="row text-center">
+                  <div className="col feature-word">Discreet</div>
+                  <div className="col feature-word">Save Lives</div>
+                  <div className="col feature-word">User-Friendly</div>
+                  <div className="col feature-word">Easily Accessible</div>
+                </div>
+              </div>
+            </div>
+
+
+
+            <div className="row align-items-center mt-5">
+                <div className="col-lg-5" data-aos="fade-right">
                     <img src="/3Dprint2.png" alt="Fenta-Nil 3D Model 2" className="solution-image2" />
                 </div>
-                <div className="col-lg-6" data-aos="fade-left">
-                    <p className="lead fs-3">Fenta-Nil leverages advanced, user-friendly technology to provide rapid results</p>
+                <div className="col-lg-6 offset-lg-1" data-aos="fade-left"> {/* Use offset to create the gap */}
+                    <p className="lead fs-1 custom-padding-right">Fenta-Nil leverages advanced technology to ensure fentanyl detection in just 2 steps</p>
                 </div>
             </div>
         </div>
